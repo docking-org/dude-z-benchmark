ef2150f431b57d269cd5558a6993f09e6d1bdfa1
Ian Knight <isknight@gimel5.cluster.ucsf.bkslab.org>
2022-12-18 23:46:40 -0800

    adds necessary post-processing to DUDE-Z INDOCK files

--- make_dataset.sh
+++ make_dataset.sh
@@ -1,2 +1,11 @@
+#!/usr/bin/env/bash
+
 wget --no-check-certificate https://dudez.docking.org/DOCKING_GRIDS_AND_POSES.tgz
 tar -xzf DOCKING_GRIDS_AND_POSES.tgz
+
+for f in DOCKING_GRIDS_AND_POSES/*/INDOCK
+do
+  sed -i -e 's/DOCK 3.7 parameter/DOCK 3.8 parameter/g' $f
+  sed -i -e 's/match_goal                    5000/match_goal                    1000/g' $f
+done
+
