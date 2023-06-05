3be1824a2feb980c2551b22db3f75c4c743f1559
Ian Knight <isk@alumni.stanford.edu>
2023-06-04 23:53:45 -0700

    removes superfluous echo statements

--- make_dataset.sh
+++ make_dataset.sh
@@ -4,7 +4,6 @@ if [[ ! -f "DOCKING_GRIDS_AND_POSES.tgz" ]]; then
 	wget --no-check-certificate https://dudez.docking.org/DOCKING_GRIDS_AND_POSES.tgz
 fi
 
-echo a
 if [[ ! -d "DOCKING_GRIDS_AND_POSES" ]]; then
 	tar -xzf DOCKING_GRIDS_AND_POSES.tgz
 	for f in DOCKING_GRIDS_AND_POSES/*/INDOCK
@@ -16,7 +15,6 @@ fi
 
 export DUDEZ_PATH=$(realpath DOCKING_GRIDS_AND_POSES/)
 
-echo b
 if [[ ! -d "property_matched" ]]; then
 	wget --no-check-certificate -nH -x --no-parent -r -l1 -A \*.tgz http://dudez.docking.org/property_matched/
 	rm -rf property_matched/*/
@@ -26,16 +24,12 @@ cd property_matched/
 for f in *.tgz
 do
 	TARGET_NAME=${f%.tgz}
-	echo c
 	if [ ! -d "$TARGET_NAME" ]; then
 		tar -xzf $f
 	fi
-	echo d
 	if [ ! -f "$DUDEZ_PATH/$TARGET_NAME/positives.tgz" ] || [ ! -f "$DUDEZ_PATH/$TARGET_NAME/negatives.tgz" ]; then
 		cd $TARGET_NAME/ligands/
-		echo e
 		tar -czf positives.tgz *.db2*
-                echo f
 		cd ../decoys/
 		tar -czf negatives.tgz *.db2*
 		cd ..
